acfe5c55eed2c446977d08e3fab498dbceb31de3
Yevheniia Kviatkovska <yevkviat@gmail.com>
2024-11-29 00:19:26 +0200

    Add lists.py

---
+++ lists.py
@@ -0,0 +1,9 @@
+# DATA STRUCTURES
+# list - sequence of mutable values
+# tuple - sequence of immutable values
+# set - collection of unique values
+# dict - collection of key-value pairs
+
+names = ["Harry", "Ron", "Hermione", "Ginny"] # list (Define a list of names)
+
+print(names)
