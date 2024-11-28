69b1a6a84e779fec5f0b6193635dd90761a75a6e
Yevheniia Kviatkovska <yevkviat@gmail.com>
2024-11-29 00:21:05 +0200

    Add comments

--- sequences.py
+++ sequences.py
@@ -6,7 +6,21 @@ print(name[2])
 print(name[3])
 print(name[4])
 
+# це послідовність список(list)
 names = ["Harry", "Ron", "Hermione"]
 
 print(names)
 print(names[0])
+
+coordinateX = 10.0
+coordinateY = 20.0
+
+# це послідовність кортеж
+coordinate = (10.0, 20.0)
+print(coordinate)
+
+# DATA STRUCTURES
+# list - sequence of mutable values
+# tuple - sequence of immutable values
+# set - collection of unique values
+# dict - collection of key-value pairs
