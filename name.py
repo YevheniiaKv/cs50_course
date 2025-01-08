00414ce822b3c06a991867ab772238f5a639cadf
Yevheniia Kviatkovska <yevkviat@gmail.com>
2025-01-09 00:20:46 +0200

    Add variable lastname

--- name.py
+++ name.py
@@ -1,3 +1,9 @@
 name = input("Name: ")
-print("Hello, " + name)
+#print("Hello, " + name)
+# print(f"Hello, {name}")
+
+lastname = input("Last name: ")
+print("Hello, " + name + lastname) #тут не буде пробілу між імʼям та прізвищем, щоб це виправити, краще використати f-рядок або поставити кому перед name та lastname
+
 print(f"Hello, {name}")
+print(f"Hello, {name} {lastname}!")
