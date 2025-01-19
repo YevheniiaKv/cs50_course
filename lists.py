b320290f47510bf095c65b5ebd5c4aca7e7456ab
Yevheniia Kviatkovska <yevkviat@gmail.com>
2025-01-19 22:45:11 +0200

    Add Script 2

--- lists.py
+++ lists.py
@@ -4,6 +4,27 @@
 # set - collection of unique values
 # dict - collection of key-value pairs
 
+# Script 1
 names = ["Harry", "Ron", "Hermione", "Ginny"] # list (Define a list of names)
 
 print(names)
+
+names.append("Draco")
+
+names.sort()
+
+print(names)
+
+# Script 2
+names2 = ["Polina", "Artem", "Varya"]
+
+print(names2)
+
+names2.append("Yasya")
+
+print(names2)
+
+names2.sort()
+print(names2)
+
+print(names, names2)
